0ee41e22cbbfb10f5b97887e705d29b45140c339
glo <glrydnlla@gmail.com>
2025-05-02 14:32:30 +0700

    add function process form (unused)

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from nltk.tokenize import sent_tokenize
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key="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",
+  api_key="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",
 )
 
 app = FastAPI()
@@ -25,12 +25,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class QAPair(BaseModel):
+class QA(BaseModel):
     question: str
     answer: str
 
 class QARequest(BaseModel):
-    qa_list: List[QAPair]
+    qa_list: List[QA]
     prompt: str
     
 class TAPair(BaseModel):
@@ -58,6 +58,16 @@ def article_to_text(article_list):
 
 def rank_to_text(softskill_list):
     return "\n".join([f"{item['rank']}. {item['softskill']}\n" for item in softskill_list])
+
+def qa_list_to_text(qa_list):
+    text = ""
+    for item in qa_list:
+        if item['question_type'] == "draggable-list":
+            ans = rank_to_text(item['answer'])
+        else:
+            ans = item['answer']
+        text += f"Q: {item['question']}\nA: {ans}\n"
+    return text
 
 @app.post("/generate-article")
 def generate_article(data: QARequest):
@@ -99,7 +109,7 @@ def summarize(data: SummaryRequest):
 
 @app.post("/find-job")
 def find_job(data: SummaryRequest):
-    text = article_to_text([ta.dict() for ta in data.article_list])
+    text = rank_to_text([ta.dict() for sf in data.softskill_list])
     prompt = data.prompt + text
     completion = client.chat.completions.create(
         model="deepseek/deepseek-r1:free",
@@ -110,3 +120,24 @@ def find_job(data: SummaryRequest):
     job_summary = completion.choices[0].message.content
     job_summary = job_summary.replace("*", "")
     return {"job_summary": job_summary}
+
+@app.post("/process-form")
+def process_form_data(data: QARequest):
+    text = qa_list_to_text([qa.dict() for qa in data.qa_list])
+    prompt = data.prompt + text
+    completion = client.chat.completions.create(
+        model="deepseek/deepseek-r1:free",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    result = completion.choices[0].message.content
+    result = result.replace("*", "")
+    
+    sentences = sent_tokenize(result)
+    title = sentences[0]
+    
+    print(title)
+    result = result.replace(title, "").lstrip()
+
+    return {"title": title, "result": result}
